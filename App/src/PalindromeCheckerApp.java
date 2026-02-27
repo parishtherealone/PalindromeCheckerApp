4d0d1ff089757adafcb7f937fb301083eda599be
Parishrut Singh <parishrutsingh314@gmail.com>
2026-02-27 16:41:10 +0530

    Write result code

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,19 @@
 class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App!");
+
+        String word = "madam";   // hardcoded string
+        String reversed = "";
+
+        // reverse the string
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        // check palindrome
+        if (word.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
